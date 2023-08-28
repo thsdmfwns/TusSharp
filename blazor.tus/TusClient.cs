31febb458daeeebf063096694dadf0135f2332c1
son <son2044@gmail.com>
2023-08-28 11:09:52 +0900

    add project

---
+++ blazor.tus/TusClient.cs
@@ -0,0 +1,25 @@
+﻿namespace blazor.tus;
+
+public class TusClient : IDisposable
+{
+    private bool _disposedValue;
+
+    
+    
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+    
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposedValue)
+        {
+            if (disposing)
+            {
+                //Todo dispose handles
+            }
+            _disposedValue = true;
+        }
+    }
+}
