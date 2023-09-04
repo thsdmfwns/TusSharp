7795f1494f58ab889d5166623bd6333ad845ecb3
son <son2044@gmail.com>
2023-09-04 13:23:59 +0900

    add TusOption

--- blazor.tus/TusClient.cs
+++ blazor.tus/TusClient.cs
@@ -1,7 +1,52 @@
-﻿namespace blazor.tus;
+﻿using blazor.tus.Constants;
+using blazor.tus.Infrastructure;
+
+namespace blazor.tus;
 
 public class TusClient
 {
     public TusUpload Upload(Stream fileStream, TusUploadOption uploadOption, CancellationToken? cancellationToken)
         => new TusUpload(fileStream, uploadOption, cancellationToken);
+
+    /// <summary>
+    ///  OPTIONS request MAY be used to gather information about the Server’s current configuration
+    /// </summary>
+    /// <param name="endPoint"></param>
+    /// <returns></returns>
+    public async Task<TusOptionResponse> SendOption(Uri endPoint)
+    {
+        using var client = new HttpClient();
+        var response = new TusOptionResponse();
+        var req = new HttpRequestMessage(HttpMethod.Options, endPoint);
+        var responseMessage = await client.SendAsync(req);
+        if (!responseMessage.TryGetValueOfHeader(TusHeaders.TusVersion, out var tusVersion)
+            || tusVersion is null)
+        {
+            throw new HttpRequestException("Invalid Tus-Version header");
+        }
+        response.TusVersion = tusVersion.Split(',').ToList();
+        
+        if (!responseMessage.TryGetValueOfHeader(TusHeaders.TusMaxSize, out var maxsizeString)
+            || maxsizeString is null
+            || !long.TryParse(maxsizeString, out var maxSize))
+        {
+            response.TusMaxSize = null;
+        }
+        else
+        {
+            response.TusMaxSize = maxSize;
+        }
+        
+        if (!responseMessage.TryGetValueOfHeader(TusHeaders.TusExtension, out var tusExtension)
+            || tusExtension is null)
+        {
+            response.TusExtension = null;
+        }
+        else
+        {
+            response.TusExtension = tusExtension.Split(',').ToList();
+        }
+
+        return response;
+    }
 }
---
+++ blazor.tus/TusOptionResponse.cs
@@ -0,0 +1,9 @@
+namespace blazor.tus;
+
+public class TusOptionResponse
+{
+    public string? TusResumable { get; set; }
+    public List<string> TusVersion { get; set; } = new List<string>();
+    public long? TusMaxSize { get; set; }
+    public List<string>? TusExtension { get; set; }
+}
