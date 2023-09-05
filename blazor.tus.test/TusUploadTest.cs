246235e0e9a68a0abf659e0db89f19ec1a0cfc3c
son <son2044@gmail.com>
2023-09-05 17:14:38 +0900

    add testcases

--- blazor.tus.test/TusUploadTest.cs
+++ blazor.tus.test/TusUploadTest.cs
@@ -7,6 +7,8 @@ namespace blazor.tus.test;
 public class TusUploadTest
 {
     private Faker _faker;
+    private readonly string _dataPath = "/mt/nvme1n1p2/tusd-data";
+    
     [SetUp]
     public void Setup()
     {
@@ -31,10 +33,14 @@ public class TusUploadTest
         };
         var fileContent = _faker.Lorem.Paragraphs();
         var stream = new MemoryStream(Encoding.UTF8.GetBytes(fileContent));
-        using var upload = client.Upload(stream, opt, null);
-        await upload.Start();
+        using var upload = client.Upload(stream, opt);
+        await upload.Start();   
         TestContext.WriteLine(err);
         Assert.That(err, Is.Null);
+        var id = opt.UploadUrl!.Segments.Last();
+        var file = new FileInfo(Path.Combine(_dataPath, id));
+        Assert.That(file.Exists, Is.True);
+        Assert.That(await File.ReadAllTextAsync(file.FullName), Is.EqualTo(fileContent));
     }
 
     [Test]
@@ -58,27 +64,18 @@ public class TusUploadTest
         };
         var fileContent = _faker.Lorem.Paragraphs();
         var stream = new MemoryStream(Encoding.UTF8.GetBytes(fileContent));
-        using (var upload = client.Upload(stream, opt, token.Token))
-        {
-            await upload.Start();
-        };   
+        using var upload = client.Upload(stream, opt);
+        await upload.Start(token.Token);
         Assert.That(opt.UploadUrl, Is.Not.Null);
         Assert.That(err, Is.Empty);
 
         err = string.Empty;
-        var resumeOpt = new TusUploadOption()
-        {
-            EndPoint = new Uri("http://172.17.0.3:1080/files"),
-            UploadUrl = opt.UploadUrl,
-            ChunkSize = 100,
-            RetryDelays = null,
-            OnFailed = (message, requestMessage, arg3, ex) => { err += $"{ex}:{arg3}\n";},
-        };
-        using (var upload = client.Upload(stream, resumeOpt, null))
-        {
-            await upload.Start();
-        }
+        await upload.Start();
         Assert.That(err, Is.Empty);
+        var id = opt.UploadUrl!.Segments.Last();
+        var file = new FileInfo(Path.Combine(_dataPath, id));
+        Assert.That(file.Exists, Is.True);
+        Assert.That(await File.ReadAllTextAsync(file.FullName), Is.EqualTo(fileContent));
     }
 
     [Test]
@@ -96,7 +93,7 @@ public class TusUploadTest
         };
         var fileContent = _faker.Lorem.Paragraphs();
         var stream = new MemoryStream(Encoding.UTF8.GetBytes(fileContent));
-        using (var upload = client.Upload(stream, opt, null))
+        using (var upload = client.Upload(stream, opt))
         {
             await upload.Start();
         }
@@ -109,6 +106,7 @@ public class TusUploadTest
     {
         var client = new TusClient();
         var retryCount = 0;
+        var completedCount = 0;
         var opt = new TusUploadOption()
         {
             EndPoint = new Uri("http://172.17.0.3:1080/files"),
@@ -118,15 +116,17 @@ public class TusUploadTest
             {
                 retryCount++;
                 return true;
-            } 
+            }, 
+            OnCompleted = () => ++completedCount,
         };
         var fileContent = _faker.Lorem.Paragraphs();
         var stream = new MemoryStream(Encoding.UTF8.GetBytes(fileContent));
-        using (var upload = client.Upload(stream, opt, null))
+        using (var upload = client.Upload(stream, opt))
         {
             await upload.Start();
         }
         Assert.That(retryCount, Is.EqualTo(opt.RetryDelays!.Count));
+        Assert.That(completedCount, Is.EqualTo(1));
     }
     
     [Test]
@@ -147,10 +147,57 @@ public class TusUploadTest
         };
         var fileContent = _faker.Lorem.Paragraphs();
         var stream = new MemoryStream(Encoding.UTF8.GetBytes(fileContent));
-        using (var upload = client.Upload(stream, opt, null))
+        using (var upload = client.Upload(stream, opt))
         {
             await upload.Start();
         }
         Assert.That(retryCount, Is.EqualTo(1));
+    }
+
+    [Test]
+    public async Task Delete()
+    {
+        var client = new TusClient();
+        var err = string.Empty;
+        var token = new CancellationTokenSource();
+        var opt = new TusUploadOption()
+        {
+            EndPoint = new Uri("http://172.17.0.3:1080/files"),
+            ChunkSize = 10,
+            RetryDelays = null,
+            OnFailed = (message, requestMessage, arg3, ex) => { err += $"{ex}"; },
+            OnProgress = (chunkSize, uploaded, total) =>
+            {
+                if (total / 3 < uploaded)
+                {
+                    token.Cancel();
+                }
+            }
+        };
+        var fileContent = _faker.Lorem.Paragraphs();
+        var stream = new MemoryStream(Encoding.UTF8.GetBytes(fileContent));
+        using var upload = client.Upload(stream, opt);
+        await upload.Start(token.Token);
+        Assert.That(opt.UploadUrl, Is.Not.Null);
+        Assert.That(err, Is.Empty);
+        var id = opt.UploadUrl!.Segments.Last();
+        var file = new FileInfo(Path.Combine(_dataPath, id));
+        Assert.That(file.Exists, Is.True);
+
+        await upload.Delete();
+        Assert.That(err, Is.Empty);
+        file = new FileInfo(Path.Combine(_dataPath, id));
+        Assert.That(file.Exists, Is.False);
+    }
+
+    [Test]
+    public async Task Option()
+    {
+        var client = new TusClient();
+        var endPoint = new Uri("http://172.17.0.3:1080/files");
+        var res = await client.SendOption(endPoint);
+        res.TusExtension!.ForEach(Console.WriteLine);
+        Assert.That(res.TusVersion, Does.Contain("1.0.0"));
+        Assert.That(res.TusExtension, Is.Not.Empty);
     }
 }
